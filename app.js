82401b49e4804045b8c74aee38a57c4f408146d4
rhema-ray <59491043+rhema-ray@users.noreply.github.com>
2022-01-15 15:54:11 +0100

    b2 commit 3

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 
+const app = app.listen(3000, ()=>{console.log("Server is running")})
 
 app.get('/', (req, res) => {
     res.send('working')
 })
 
 
-app.listen(3000, ()=>{
-    console.log("Server is running")
-})
+/*const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); */
